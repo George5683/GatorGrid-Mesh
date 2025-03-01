117cae6cecef1861dab82524f940192096c466df
George Salomon <newtech20111@hotmail.com>
2025-03-01 14:58:01 -0500

    working on the coding...

--- examples/picow_access_point/picow_access_point.c
+++ examples/picow_access_point/picow_access_point.c
@@ -1,94 +1,19 @@
-# include "cyw43.hpp"
-#include <stdio.h>
-#include "pico/stdlib.h"
-#include "pico/cyw43_arch.h"
-#include "hardware/vreg.h"
-#include "hardware/clocks.h"
+/**
+ * Copyright (c) 2022 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
 #include <string.h>
+
+#include "pico/cyw43_arch.h"
+#include "pico/stdlib.h"
+
 #include "lwip/pbuf.h"
 #include "lwip/tcp.h"
+
 #include "dhcpserver.h"
 #include "dnsserver.h"
-
-static int scan_result(void *env, const cyw43_ev_scan_result_t *result) {
-    if (result) {
-        printf("ssid: %-32s rssi: %4d chan: %3d mac: %02x:%02x:%02x:%02x:%02x:%02x sec: %u\n",
-            result->ssid, result->rssi, result->channel,
-            result->bssid[0], result->bssid[1], result->bssid[2], result->bssid[3], result->bssid[4], result->bssid[5],
-            result->auth_mode);
-    }
-    return 0;
-}
-
-// Initialize the driver for the wifi
-bool cyw43::init(){
-    if(!cyw43_arch_init()){
-        printf("Failed to initiate cyw43 driver...\n");
-        return false;
-    };
-
-    return true;
-}
-
-// MUST BE IN STA TO CONNECT TO A NETWORK
-bool cyw43::sta_mode_enable(){
-    cyw43_arch_enable_sta_mode();
-}
-
-void scan_for_wifi(int delayms){
-    absolute_time_t scan_time = nil_time;
-    bool scan_in_progress = false;
-    while(true) {
-        if (absolute_time_diff_us(get_absolute_time(), scan_time) < 0) {
-            if (!scan_in_progress) {
-                cyw43_wifi_scan_options_t scan_options = {0};
-                int err = cyw43_wifi_scan(&cyw43_state, &scan_options, NULL, scan_result);
-                if (err == 0) {
-                    printf("\nPerforming wifi scan\n");
-                    scan_in_progress = true;
-                } else {
-                    printf("Failed to start scan: %d\n", err);
-                    scan_time = make_timeout_time_ms(delayms); // wait 10s and scan again
-                }
-            } else if (!cyw43_wifi_scan_active(&cyw43_state)) {
-                scan_time = make_timeout_time_ms(delayms); // wait 10s and scan again
-                scan_in_progress = false; 
-            }
-        }
-        // the following #ifdef is only here so this same example can be used in multiple modes;
-        // you do not need it in your code
-        #if PICO_CYW43_ARCH_POLL
-                // if you are using pico_cyw43_arch_poll, then you must poll periodically from your
-                // main loop (not from a timer) to check for Wi-Fi driver or lwIP work that needs to be done.
-                cyw43_arch_poll();
-                // you can poll as often as you like, however if you have nothing else to do you can
-                // choose to sleep until either a specified time, or cyw43_arch_poll() has work to do:
-                cyw43_arch_wait_for_work_until(scan_time);
-        #else
-        // if you are not using pico_cyw43_arch_poll, then WiFI driver and lwIP work
-        // is done via interrupt in the background. This sleep is just an example of some (blocking)
-        // work you might be doing.
-        sleep_ms(1000);
-        #endif
-    }
-}
-
-// MUST BE IN AP MODE TO HOST A NETWORK
-bool cyw43::ap_mode_enable(const char *ssid, const char *password, uint32_t auth){
-    cyw43_arch_enable_ap_mode(ssid, password, auth);
-}
-
-// Enable bluetooth 
-bool cyw43::bluetooth_enable(){
-    // To enable bluetooth, you need to turn it on in the config of cyw43.h 
-    // CYW43_ENABLE_BLUETOOTH (0)
-    // or 
-    // add this to cmake
-    // add_compile_definitions(CYW43_ENABLE_BLUETOOTH=1)
-    
-    
-    return true;
-}
 
 #define TCP_PORT 80
 #define DEBUG_printf printf
@@ -355,9 +280,79 @@ void key_pressed_func(void *param) {
     }
 }
 
-
-int main(){
-    // initialize everything
+int main() {
     stdio_init_all();
 
+    TCP_SERVER_T *state = calloc(1, sizeof(TCP_SERVER_T));
+    if (!state) {
+        DEBUG_printf("failed to allocate state\n");
+        return 1;
+    }
+
+    if (cyw43_arch_init()) {
+        DEBUG_printf("failed to initialise\n");
+        return 1;
+    }
+
+    // Get notified if the user presses a key
+    stdio_set_chars_available_callback(key_pressed_func, state);
+
+    const char *ap_name = "picow_test";
+#if 1
+    const char *password = "password";
+#else
+    const char *password = NULL;
+#endif
+
+    cyw43_arch_enable_ap_mode(ap_name, password, CYW43_AUTH_WPA2_AES_PSK);
+
+    #if LWIP_IPV6
+    #define IP(x) ((x).u_addr.ip4)
+    #else
+    #define IP(x) (x)
+    #endif
+
+    ip4_addr_t mask;
+    IP(state->gw).addr = PP_HTONL(CYW43_DEFAULT_IP_AP_ADDRESS);
+    IP(mask).addr = PP_HTONL(CYW43_DEFAULT_IP_MASK);
+
+    #undef IP
+
+    // Start the dhcp server
+    dhcp_server_t dhcp_server;
+    dhcp_server_init(&dhcp_server, &state->gw, &mask);
+
+    // Start the dns server
+    dns_server_t dns_server;
+    dns_server_init(&dns_server, &state->gw);
+
+    if (!tcp_server_open(state, ap_name)) {
+        DEBUG_printf("failed to open server\n");
+        return 1;
+    }
+
+    state->complete = false;
+    while(!state->complete) {
+        // the following #ifdef is only here so this same example can be used in multiple modes;
+        // you do not need it in your code
+#if PICO_CYW43_ARCH_POLL
+        // if you are using pico_cyw43_arch_poll, then you must poll periodically from your
+        // main loop (not from a timer interrupt) to check for Wi-Fi driver or lwIP work that needs to be done.
+        cyw43_arch_poll();
+        // you can poll as often as you like, however if you have nothing else to do you can
+        // choose to sleep until either a specified time, or cyw43_arch_poll() has work to do:
+        cyw43_arch_wait_for_work_until(make_timeout_time_ms(1000));
+#else
+        // if you are not using pico_cyw43_arch_poll, then Wi-FI driver and lwIP work
+        // is done via interrupt in the background. This sleep is just an example of some (blocking)
+        // work you might be doing.
+        sleep_ms(1000);
+#endif
+    }
+    tcp_server_close(state);
+    dns_server_deinit(&dns_server);
+    dhcp_server_deinit(&dhcp_server);
+    cyw43_arch_deinit();
+    printf("Test complete\n");
+    return 0;
 }
--- libraries/cyw43.hpp
+++ libraries/cyw43.hpp
@@ -2,7 +2,7 @@ class cyw43{
     public:
         bool sta_mode_enable();
 
-        bool ap_mode_enable();
+        bool ap_mode_enable(const char *ssid, const char *password, uint32_t auth);
 
         bool init();
 
