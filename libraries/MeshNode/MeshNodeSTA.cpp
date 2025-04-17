1873db2952ed31cb4d3b27ec89f61a43e1ec6595
Jackb18b <jackb18b@gmail.com>
2025-04-17 17:08:41 -0400

    hahahahahahha

--- libraries/MeshNode/MeshNodeSTA.cpp
+++ libraries/MeshNode/MeshNodeSTA.cpp
@@ -604,7 +604,7 @@ bool STANode::handle_incoming_data(unsigned char* buffer, struct pbuf *p) {
     if (ACK_flag && !self_reply){
         TCP_ACK_MESSAGE ackMsg(get_NodeID(), source_id, p->tot_len);
         send_tcp_data(ackMsg.get_msg(), ackMsg.get_len(), false);
-        state->waiting_for_ack = true;
+        //state->waiting_for_ack = true;
     } else if (NAK_flag) {
         // TODO: Update for error handling
         // identify the source from sender and send back?
