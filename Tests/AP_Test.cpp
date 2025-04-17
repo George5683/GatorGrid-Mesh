f2c9f229e1734d399a0288a7ccb99873795d1a2a
Jackb18b <jackb18b@gmail.com>
2025-04-17 17:45:54 -0400

    fixing ACK waiting

--- Tests/AP_Test.cpp
+++ Tests/AP_Test.cpp
@@ -15,7 +15,7 @@ int main() {
     sleep_ms(10000);
 
     //multicore_launch_core1(core1_entry);
-    APNode node;
+    APNode node(0);
 
     printf("Init AP Mode\n");
     if (!node.init_ap_mode())
