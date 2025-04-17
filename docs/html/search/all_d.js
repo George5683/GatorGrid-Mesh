7a5be0115c17123809cc5937b36e7ba9d4f2be5d
mateo <teoslivka@gmail.com>
2025-04-17 11:21:39 -0400

    spi and meshnode docs updated

---
+++ docs/html/search/all_d.js
@@ -0,0 +1,15 @@
+var searchData=
+[
+  ['tcp_5fack_5fmessage_0',['TCP_ACK_MESSAGE',['../classTCP__ACK__MESSAGE.html',1,'']]],
+  ['tcp_5fclient_5ft_5f_1',['TCP_CLIENT_T_',['../structTCP__CLIENT__T__.html',1,'']]],
+  ['tcp_5fconnect_5fstate_5ft_5f_2',['TCP_CONNECT_STATE_T_',['../structTCP__CONNECT__STATE__T__.html',1,'']]],
+  ['tcp_5fdata_5fmsg_3',['TCP_DATA_MSG',['../classTCP__DATA__MSG.html',1,'']]],
+  ['tcp_5fdisconnect_5fmsg_4',['TCP_DISCONNECT_MSG',['../classTCP__DISCONNECT__MSG.html',1,'']]],
+  ['tcp_5finit_5fmessage_5',['TCP_INIT_MESSAGE',['../classTCP__INIT__MESSAGE.html',1,'']]],
+  ['tcp_5fmessage_6',['TCP_MESSAGE',['../classTCP__MESSAGE.html',1,'']]],
+  ['tcp_5fnak_5fmessage_7',['TCP_NAK_MESSAGE',['../classTCP__NAK__MESSAGE.html',1,'']]],
+  ['tcp_5fserver_5ft_5f_8',['TCP_SERVER_T_',['../structTCP__SERVER__T__.html',1,'']]],
+  ['tcp_5fupdate_5fmessage_9',['TCP_UPDATE_MESSAGE',['../classTCP__UPDATE__MESSAGE.html',1,'']]],
+  ['tt_20bool_20init_5fap_5fmode_20tt_10',['&lt;tt&gt;bool init_ap_mode()&lt;/tt&gt;',['../index.html#autotoc_md6',1,'']]],
+  ['tt_20void_20meshnode_20void_20tt_11',['&lt;tt&gt;void MeshNode(void)&lt;/tt&gt;',['../index.html#autotoc_md4',1,'']]]
+];
