dcf0f978859b5ff6b9e133522dfb4efa4d334613
Jackb18b <jackb18b@gmail.com>
2025-04-21 19:05:24 -0400

    :facepalm:

--- Tests/sta_test.cpp
+++ Tests/sta_test.cpp
@@ -125,7 +125,7 @@ int main() {
         TCP_DATA_MSG msg(node.get_NodeID(), node.parent);
         uint8_t arr[] = "hello, this is message:  ";
         arr[24] = 48 + i;
-        msg.add_message(25, arr);
+        msg.add_message(arr, 25);
         node.send_tcp_data(msg.get_msg(), msg.get_len(), false);
     }
 
