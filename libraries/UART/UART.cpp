3ab293e7043c23881dbb044fdb5cb25dd956b825
Maxwell Evans <116737758+ToTheMax24@users.noreply.github.com>
2025-09-16 17:27:11 -0400

    Update UART.cpp

--- libraries/UART/UART.cpp
+++ libraries/UART/UART.cpp
@@ -1,64 +1,68 @@
-#include "UART.hpp"
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/uart.h"
+#include "picoUARTLibrary.hpp"
 
-PicoUART* PicoUART::instance = nullptr;
+#define UART_ID    uart0
+#define BAUD_RATE  115200
+#define DATA_BITS  8
+#define STOP_BITS  1
+#define PARITY     UART_PARITY_NONE
 
-// Constructor
-PicoUART::PicoUART() {
-    // Empty constructor
-}
+#define UART_TX_PIN 16
+#define UART_RX_PIN 17
 
-// Initialize UART hardware
-bool PicoUART::picoUartInit() {
-    uart_init(UART_ID, BAUD_RATE);
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
+#define MAX_LEN 128
 
-    uart_set_hw_flow(UART_ID, false, false);
-    uart_set_format(UART_ID, DATA_BITS, STOP_BITS, PARITY);
-    uart_set_fifo_enabled(UART_ID, true);
+char rxBuffer[MAX_LEN];
+int rxIndex = 0;
+volatile bool messageReady = false;
 
-    return true;
-}
+void on_uart_rx(void) {
 
-// Initialize UART interrupt
-bool PicoUART::picoUARTInterruptInit() {
-    instance = this;
-
-    int UART_IRQ = (UART_ID == uart0) ? UART0_IRQ : UART1_IRQ;
-    irq_set_exclusive_handler(UART_IRQ, on_uart_rx);
-    irq_set_enabled(UART_IRQ, true);
-    uart_set_irq_enables(UART_ID, true, false);
-
-    return true;
-}
-
-// Send message over UART
-int PicoUART::sendMessage(const char* message) {
-    while (!uart_is_writable(UART_ID)) {
-        tight_loop_contents();
-    }
-    uart_puts(UART_ID, message);
-    return 0;
-}
-
-// Return pointer to received buffer
-char* PicoUART::getReadBuffer() {
-    return rxBuffer;
-}
-
-// Static ISR
-void PicoUART::on_uart_rx() {
-    if (!instance) return;
+    printf("Interrupt\n");
 
     while (uart_is_readable(UART_ID)) {
         char c = uart_getc(UART_ID);
-        if (instance->rxIndex < MAX_LEN - 1) {
-            instance->rxBuffer[instance->rxIndex++] = c;
-            instance->rxBuffer[instance->rxIndex] = '\0';
+
+        if (rxIndex < MAX_LEN - 1) {
+            rxBuffer[rxIndex++] = c;
+            rxBuffer[rxIndex] = '\0';
         }
 
         if (c == '\n') {
-            instance->rxIndex = 0;
+            messageReady = true;
+            rxIndex = 0;
         }
+    }
+}
+
+int main() {
+    stdio_init_all();
+
+    PicoUART uart;
+
+    uart.picoUARTInit();
+    uart.picoUARTInterruptInit();
+    printf("UART Initialized\n");
+    
+    sleep_ms(2000);
+
+    while (true) {
+        if (messageReady) {
+
+            printf("Received: %s", rxBuffer);
+
+            while (!uart_is_writable(UART_ID)) {
+                tight_loop_contents();
+            }
+
+            uart_puts(UART_ID, "Herobrine\n");
+            printf("Sending: Herobrine\n");
+
+            messageReady = false;
+        }
+
+        sleep_ms(50);
     }
 }
